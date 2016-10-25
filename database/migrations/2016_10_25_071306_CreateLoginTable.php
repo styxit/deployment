ed783d6aecd219974709feb1c8370ca9a366ea93
Styxit <info@styxit.com>
2016-10-25 12:07:46 +0200

    User table migration

---
+++ database/migrations/2016_10_25_071306_CreateLoginTable.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoginTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+
+            // Cached from GitHub.
+            $table->string('github_id')->unique();
+            $table->string('login');
+            $table->string('name');
+            $table->string('email');
+            $table->string('token');
+            $table->string('avatar');
+
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
