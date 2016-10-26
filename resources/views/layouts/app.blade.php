36bd0ff5b7faefa4ae8f5ae2d41845966c3785b0
Styxit <info@styxit.com>
2016-10-26 13:59:52 +0200

    Add a layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Styxit">
+    <meta name="keyword" content="Github, Deployments">
+    <link rel="shortcut icon" href="img/favicon.png">
+
+    <title>Deployments - @yield('title')</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap-reset.css') }}" rel="stylesheet">
+
+    <!--external css-->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style-responsive.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+<section id="container" class="">
+    <!--header start-->
+    <header class="header white-bg">
+        <div class="sidebar-toggle-box">
+            <div data-original-title="Toggle Navigation" data-placement="right" class="icon-reorder tooltips"></div>
+        </div>
+        <!--logo start-->
+        <a href="/home" class="logo" >Deployment <span>statuses</span></a>
+        <!--logo end-->
+        <div class="top-nav ">
+            <ul class="nav pull-right top-menu">
+                <li>
+                    <input type="text" class="form-control search" placeholder="Search">
+                </li>
+                <!-- user login dropdown start-->
+                <li class="dropdown">
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                        <img alt="" src="{{ Auth::user()->avatar }}" width="29">
+                        <span class="username">{{ Auth::user()->name }}</span>
+                        <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu extended logout">
+                        <div class="log-arrow-up"></div>
+                        <li><a href="#"><i class="fa fa-suitcase" aria-hidden="true"></i>Profile</a></li>
+                        <li><a href="#"><i class="fa fa-cog" aria-hidden="true"></i></i> Settings</a></li>
+                        <li><a href="#"><i class="fa fa-bell" aria-hidden="true"></i> Notification</a></li>
+                        <li><a href="/logout"><i class="fa fa-key" aria-hidden="true"></i> Log Out</a></li>
+                    </ul>
+                </li>
+                <!-- user login dropdown end -->
+            </ul>
+        </div>
+    </header>
+    <!--header end-->
+    <!--sidebar start-->
+    <aside>
+        <div id="sidebar"  class="nav-collapse ">
+            <!-- sidebar menu start-->
+            <ul class="sidebar-menu" id="nav-accordion">
+                <li>
+                    <a href="/home">
+                        <i class="fa fa-dashboard" aria-hidden="true"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="sub-menu">
+                    <a href="/repositories">
+                        <i class="fa fa-laptop" aria-hidden="true"></i>
+                        <span>Repositories</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a  href="/deployments">
+                        <i class="fa fa-user" aria-hidden="true"></i>
+                        <span>Deployments</span>
+                    </a>
+                </li>
+            </ul>
+            <!-- sidebar menu end-->
+        </div>
+    </aside>
+    <!--sidebar end-->
+    <!--main content start-->
+    <section id="main-content">
+        <section class="wrapper site-min-height">
+            <!-- page start-->
+            @yield('content')
+            <!-- page end-->
+        </section>
+    </section>
+    <!--main content end-->
+    <!--footer start-->
+    <footer class="site-footer">
+        <div class="text-center">
+            {{ date('Y') }} &copy; Deployment statuses.
+            <a href="#" class="go-top">
+                <i class="fa fa-angle-up" aria-hidden="true"></i>
+            </a>
+        </div>
+    </footer>
+    <!--footer end-->
+</section>
+
+<!-- js placed at the end of the document so the pages load faster -->
+<script src="{{ asset('js/jquery.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/respond.min.js') }}" ></script>
+
+<!--common script for all pages-->
+<script src="{{ asset('js/common-scripts.js') }}"></script>
+
+</body>
+</html>
